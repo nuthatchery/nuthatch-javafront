0cae4fc6b8c516fae685bfc708ffc9362af878fe
Anya Helene Bagge <anya@ii.uib.no>
2013-01-30 13:35:29 +0100

    Make a who-calls-who example

--- src/nuthatch/demo/ExceptionAnalysis.java
+++ src/nuthatch/demo/ExceptionAnalysis.java
@@ -2,18 +2,24 @@ package nuthatch.demo;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
-import nuthatch.engine.Engine;
-import nuthatch.engine.impl.StrategicEngine;
 import nuthatch.javafront.JavaPatterns;
 import nuthatch.library.strategies.Visitor;
 import static nuthatch.javafront.JavaPatterns.*;
 import nuthatch.pattern.Environment;
 import nuthatch.pattern.EnvironmentFactory;
 import nuthatch.pattern.Pattern;
+import nuthatch.pattern.impl.Var;
 import nuthatch.stratego.adapter.StrategoAdapter;
 import nuthatch.stratego.adapter.TermEngine;
 import nuthatch.stratego.pattern.TermPatternFactory;
+import nuthatch.strategy.Before;
+import nuthatch.strategy.Strategy;
 import nuthatch.tree.TreeCursor;
 
 import org.spoofax.interpreter.terms.IStrategoTerm;
@@ -39,25 +45,55 @@ public class ExceptionAnalysis {
 	public static void main(String[] args) throws TokenExpectedException, BadTokenException, ParseException, ParseError, SGLRException, IOException, InvalidParseTableException {
 
 		IStrategoTerm term = StrategoAdapter.parseFile("/tmp/RationalValue.java", getJavaParseTable());
+		final Map<String, List<String>> resultMap = new HashMap<String, List<String>>();
 		
 		Visitor<TermEngine> visitor = new Visitor<TermEngine>() {
 			@Override
 			public void onEntry(TermEngine e) {
 				Environment<TreeCursor<IStrategoTerm, Integer>> env = EnvironmentFactory.env();
-
-				if(ClassBody(var("body")).match(e, env)) {
+				if(Invoke(Method(MethodName(var("name"))), var("args")).match(e, env)) {
+					List<String> list = resultMap.get(e.getS());
+					if(list == null) {
+						list = new ArrayList<String>();
+						resultMap.put(e.getS(), list);
+					}
+					list.add(getName(env.get("name")));
+				}
+				/*if(ClassBody(var("body")).match(e, env)) {
 					for(TreeCursor<IStrategoTerm, Integer> child : env.get("body")) {
 						System.out.println(child.treeToString());
 					}
 				}
-				else if(MethodDec(MethodDecHead(var("modifiers"),var("typeParams"),var("retType"),var("methodName"),var("params"),var("throws")),var("body")).match(e, env)) {
-					System.out.println(env.get("methodName").treeToString());
-				}
+				/*else if(MethodDec(MethodDecHead(var("modifiers"),var("typeParams"),var("retType"),var("methodName"),var("params"),var("throws")),var("body")).match(e, env)) {
+					e.setS(getName(env.get("methodName")));
+				}*/
 			}
 		};
 		
-		TermEngine engine = new TermEngine(StrategoAdapter.termToTree(term), visitor);
+		Strategy<TermEngine> nameTracker = new Before<TermEngine>(visitor) {
+			@Override
+			public int visitBefore(TermEngine e) {
+				Environment<TreeCursor<IStrategoTerm, Integer>> env = EnvironmentFactory.env();	
+				if(MethodDec(MethodDecHead(var("modifiers"),var("typeParams"),var("retType"),var("methodName"),var("params"),var("throws")),var("body")).match(e, env)) {
+					e.setS(getName(env.get("methodName")));
+				}
+				else if(ConstrDec(ConstrDecHead(var("modifiers"),var("typeParams"),var("methodName"),var("params"),var("throws")),var("body")).match(e, env)) {
+					e.setS(getName(env.get("methodName")));
+				}
+				return PROCEED;
+			}
+			
+		};
+
+		TermEngine engine = new TermEngine(StrategoAdapter.termToTree(term), nameTracker);
 		engine.engage();
+		
+		for(Entry<String, List<String>> entry : resultMap.entrySet()) {
+			System.out.printf("%25s calls: ", entry.getKey());
+			for(String s : entry.getValue())
+				System.out.print(s + " ");
+			System.out.println();
+		}
 	}
 	
 	public static ParseTable getJavaParseTable() throws ParseError, IOException, InvalidParseTableException {
@@ -68,5 +104,19 @@ public class ExceptionAnalysis {
 
 	public static Pattern<IStrategoTerm, Integer> var(String name) {
 		return TermPatternFactory.getInstance().var(name);
+	}
+	
+	public static String getName(TreeCursor<IStrategoTerm, Integer> tree) {
+		Var<IStrategoTerm, Integer> s = new Var<>();
+		Var<IStrategoTerm, Integer> t = new Var<>();
+		if(Id(s).match(tree)) {
+			return s.get().getName();
+		}
+		else if(AmbName(s, t).match(tree)) {
+			return getName(s.get()) + "." + getName(t.get());
+		}
+		else {
+			throw new RuntimeException("Unknown name: " + tree.treeToString());
+		}
 	}
 }
