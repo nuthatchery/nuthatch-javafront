868592cea86c004546f8f47e8285f554d9ba7641
Anya Helene Bagge <anya@ii.uib.no>
2013-02-02 22:51:33 +0100

    FindBugs fixes; set formatting profile

--- src/nuthatch/demo/CallAnalysis.java
+++ src/nuthatch/demo/CallAnalysis.java
@@ -48,13 +48,13 @@ import org.spoofax.terms.ParseError;
 
 public class CallAnalysis {
 
-	public static ParseTable getJavaParseTable() throws ParseError,
-			IOException, InvalidParseTableException {
-		InputStream stream = JavaPatterns.class
-				.getResourceAsStream("syntax/JavaCompilationUnit-15.tbl");
+	public static ParseTable getJavaParseTable() throws ParseError, IOException, InvalidParseTableException {
+		try (InputStream stream = JavaPatterns.class.getResourceAsStream("syntax/JavaCompilationUnit-15.tbl")) {
 
-		return StrategoAdapter.getParseTableManager().loadFromStream(stream);
+			return StrategoAdapter.getParseTableManager().loadFromStream(stream);
+		}
 	}
+
 
 	/**
 	 * @param args
@@ -66,39 +66,38 @@ public class CallAnalysis {
 	 * @throws BadTokenException
 	 * @throws TokenExpectedException
 	 */
-	public static void main(String[] args) throws TokenExpectedException,
-			BadTokenException, ParseException, ParseError, SGLRException,
-			IOException, InvalidParseTableException {
-		IStrategoTerm term = StrategoAdapter.parseFile(
-				"/tmp/RationalValue.java", getJavaParseTable());
+	public static void main(String[] args) throws TokenExpectedException, BadTokenException, ParseException, ParseError, SGLRException, IOException, InvalidParseTableException {
+		IStrategoTerm term = StrategoAdapter.parseFile("/tmp/RationalValue.java", getJavaParseTable());
 		final MultiMap<String, String> resultMap = aspectVariant(term);
 		final MultiMap<String, String> resultMap2 = ancestorVariant(term);
 
-		for (Entry<String, List<String>> entry : resultMap.entrySet()) {
+		for(Entry<String, List<String>> entry : resultMap.entrySet()) {
 			System.out.printf("%25s calls: ", entry.getKey());
-			for (String s : entry.getValue()) {
+			for(String s : entry.getValue()) {
 				System.out.print(s + " ");
 			}
 			System.out.println();
 		}
 
-		System.out.println("aspectVariant(t).equals(ancestorVariant(t)) = "
-				+ resultMap.equals(resultMap2));
+		System.out.println("aspectVariant(t).equals(ancestorVariant(t)) = " + resultMap.equals(resultMap2));
 	}
 
+
 	public static String nameToStr(TreeCursor<IStrategoTerm, Integer> tree) {
-		Environment<TreeCursor<IStrategoTerm, Integer>> env = EnvironmentFactory
-				.env();
+		Environment<TreeCursor<IStrategoTerm, Integer>> env = EnvironmentFactory.env();
 		TermVar s = new TermVar(env);
 		TermVar t = new TermVar(env);
-		if (Id(s).match(tree, env)) {
+		if(Id(s).match(tree, env)) {
 			return s.get().getName();
-		} else if (AmbName(s, t).match(tree, env)) {
+		}
+		else if(AmbName(s, t).match(tree, env)) {
 			return nameToStr(s.get()) + "." + nameToStr(t.get());
-		} else {
+		}
+		else {
 			throw new RuntimeException("Unknown name: " + tree.treeToString());
 		}
 	}
+
 
 	private static MultiMap<String, String> ancestorVariant(IStrategoTerm term) {
 		final MultiMap<String, String> resultMap = new MultiMap<String, String>();
@@ -106,22 +105,19 @@ public class CallAnalysis {
 		Strategy<TermEngine> calls = new Visitor<TermEngine>() {
 			@Override
 			public void onEntry(TermEngine e) {
-				Environment<TreeCursor<IStrategoTerm, Integer>> env = EnvironmentFactory
-						.env();
-				if (Invoke(Method(MethodName(var("name"))), var("args")).match(
-						e, env)) {
-					resultMap.add(getSurroundingMethodName(e),
-							nameToStr(env.get("name")));
+				Environment<TreeCursor<IStrategoTerm, Integer>> env = EnvironmentFactory.env();
+				if(Invoke(Method(MethodName(var("name"))), var("args")).match(e, env)) {
+					resultMap.add(getSurroundingMethodName(e), nameToStr(env.get("name")));
 				}
 			}
 
 		};
-		TermEngine engine = new TermEngine(StrategoAdapter.termToTree(term),
-				calls);
+		TermEngine engine = new TermEngine(StrategoAdapter.termToTree(term), calls);
 		engine.engage();
 
 		return resultMap;
 	}
+
 
 	private static MultiMap<String, String> aspectVariant(IStrategoTerm term) {
 		final MultiMap<String, String> resultMap = new MultiMap<String, String>();
@@ -130,71 +126,65 @@ public class CallAnalysis {
 		Visitor<TermEngine> visitor = new Visitor<TermEngine>() {
 			@Override
 			public void onEntry(TermEngine e) {
-				Environment<TreeCursor<IStrategoTerm, Integer>> env = EnvironmentFactory
-						.env();
-				if (Invoke(Method(MethodName(var("name"))), var("args")).match(
-						e, env)) {
-					resultMap.add(nameToStr(e.getSubtreeVar(scopeName)),
-							nameToStr(env.get("name")));
+				Environment<TreeCursor<IStrategoTerm, Integer>> env = EnvironmentFactory.env();
+				if(Invoke(Method(MethodName(var("name"))), var("args")).match(e, env)) {
+					resultMap.add(nameToStr(e.getSubtreeVar(scopeName)), nameToStr(env.get("name")));
 				}
 			}
 		};
 
-		Strategy<TermEngine> nameTracker = new VisitorAspect<TermEngine>(
-				visitor) {
+		Strategy<TermEngine> nameTracker = new VisitorAspect<TermEngine>(visitor) {
 
 			@Override
 			public void beforeEntry(TermEngine e) {
-				Environment<TreeCursor<IStrategoTerm, Integer>> env = EnvironmentFactory
-						.env();
+				Environment<TreeCursor<IStrategoTerm, Integer>> env = EnvironmentFactory.env();
 				TermVar name = new TermVar(env);
 
-				if (MethodDec(var("head", MethodDecHead(_, _, _, name, _, _)),
-						_).match(e, env)) {
+				if(MethodDec(var("head", MethodDecHead(_, _, _, name, _, _)), _).match(e, env)) {
 					e.setSubtreeVar(scopeName, name.get());
-				} else if (ConstrDec(
-						var("head", ConstrDecHead(_, _, name, _, _)), _).match(
-						e, env)) {
+				}
+				else if(ConstrDec(var("head", ConstrDecHead(_, _, name, _, _)), _).match(e, env)) {
 					e.setSubtreeVar(scopeName, name.get());
 				}
 			}
 
 		};
 
-		TermEngine engine = new TermEngine(StrategoAdapter.termToTree(term),
-				nameTracker);
+		TermEngine engine = new TermEngine(StrategoAdapter.termToTree(term), nameTracker);
 		engine.engage();
 		return resultMap;
 	}
 
+
 	private static String getSurroundingMethodName(TermEngine e) {
-		Environment<TreeCursor<IStrategoTerm, Integer>> env = EnvironmentFactory
-				.env();
-		if (ancestor(
-				or(MethodDec(MethodDecHead(_, _, _, var("scopeName"), _, _), _),
-						ConstrDec(ConstrDecHead(_, _, var("scopeName"), _, _),
-								_))).match(e, env)) {
+		Environment<TreeCursor<IStrategoTerm, Integer>> env = EnvironmentFactory.env();
+		if(ancestor(or(MethodDec(MethodDecHead(_, _, _, var("scopeName"), _, _), _), ConstrDec(ConstrDecHead(_, _, var("scopeName"), _, _), _))).match(e, env)) {
 			return nameToStr(env.get("scopeName"));
-		} else {
+		}
+		else {
 			return null;
 		}
 	}
 
+
 	static class MultiMap<K, V> {
 		Map<K, List<V>> resultMap = new HashMap<K, List<V>>();
 
+
 		public void add(K key, V value) {
 			List<V> list = resultMap.get(key);
-			if (list == null) {
+			if(list == null) {
 				list = new ArrayList<V>();
 				resultMap.put(key, list);
 			}
 			list.add(value);
 		}
 
+
 		public Set<Entry<K, List<V>>> entrySet() {
 			return resultMap.entrySet();
 		}
+
 
 		/*
 		 * (non-Javadoc)
@@ -203,25 +193,27 @@ public class CallAnalysis {
 		 */
 		@Override
 		public boolean equals(Object obj) {
-			if (this == obj) {
+			if(this == obj) {
 				return true;
 			}
-			if (obj == null) {
+			if(obj == null) {
 				return false;
 			}
-			if (getClass() != obj.getClass()) {
+			if(getClass() != obj.getClass()) {
 				return false;
 			}
 			MultiMap<?, ?> other = (MultiMap<?, ?>) obj;
-			if (resultMap == null) {
-				if (other.resultMap != null) {
+			if(resultMap == null) {
+				if(other.resultMap != null) {
 					return false;
 				}
-			} else if (!resultMap.equals(other.resultMap)) {
+			}
+			else if(!resultMap.equals(other.resultMap)) {
 				return false;
 			}
 			return true;
 		}
+
 
 		/*
 		 * (non-Javadoc)
@@ -232,8 +224,7 @@ public class CallAnalysis {
 		public int hashCode() {
 			final int prime = 31;
 			int result = 1;
-			result = prime * result
-					+ ((resultMap == null) ? 0 : resultMap.hashCode());
+			result = prime * result + (resultMap == null ? 0 : resultMap.hashCode());
 			return result;
 		}
 	}
